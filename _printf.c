61e41976c2c7acd6a53c12e135855b5f4bb2a53b
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-26 16:52:30 -0400

    Coding printf

--- _printf.c
+++ _printf.c
@@ -6,39 +6,49 @@ int print_int(int num);
 
 int _printf(const char *format, ...)
 {
-    va_list Mylist;
-    int p_char = 0;
+	va_list Mylist;
+	int p_char = 0;
 
-    va_start(Mylist, format);
+	va_start(Mylist, format);
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+	{
+		return (-1);
+	}
 
-    while (*format) {
-        if (*format == '%') {
-            format++;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
 
-            if (*format == 'c') {
-                char c = va_arg(Mylist, int);
-                p_char += print_char(c);
-            } else if (*format == 's') {
-                char *str = va_arg(Mylist, char*);
-                p_char += print_str(str);
-            } else if (*format == 'd' || *format == 'i') {
-                int num = va_arg(Mylist, int);
-                p_char += print_int(num);
-            } else {
-                write(1, format, 1);
-                p_char++;
-            }
-        } else {
-            write(1, format, 1);
-            p_char++;
-        }
+			if (*format == 'c')
+			{
+				p_char += print_char(va_arg(Mylist, int));
+			}
+			else if (*format == 's')
+			{
+				p_char += print_str(va_arg(Mylist, char*));
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				p_char += print_int(va_arg(Mylist, int));
+			}
+			else
+			{
+				write(1, format, 1);
+				p_char++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			p_char++;
+		}
 
-        format++;
-    }
+		format++;
+	}
 
-    va_end(Mylist);
-    return p_char;
+	va_end(Mylist);
+	return (p_char);
 }
